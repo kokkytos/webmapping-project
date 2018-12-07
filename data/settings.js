a8e5ad5a61fedbef27e7a37a6485fa2269aaca2e
Leonidas Liakos <leonidas_liakos@yahoo.gr>
2018-12-07 14:26:01 +0200

    Initial commit

---
+++ data/settings.js
@@ -0,0 +1,26 @@
+var period={
+    minYear: 1950,
+    maxYear: 1975,
+}
+
+ var labels = {
+    pois: "Places",
+    grayscale: "Grayscale",
+    streets: "Streets",
+    datayear: "Data for year: ",
+    dataperiod:"Data for period: ",
+    points: "Points:" ,
+
+
+aa: "ID" , 
+ID: "ID2" , 
+F2:"F2",
+Last_name:"Last name", 
+First_name:"First name", 
+Year_of_bi:"Year of birth", 
+F6:"F6", 
+Place_of_o:"Place of origin", 
+First_plac:"First place", 
+Current_pl:"Current place"
+
+};
